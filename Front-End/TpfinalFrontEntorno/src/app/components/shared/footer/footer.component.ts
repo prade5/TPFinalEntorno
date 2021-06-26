030abdd0a0c57b574d25c4c16ea840afe7eae9db
juliiian99 <64239565+juliiian99@users.noreply.github.com>
2021-06-26 12:12:45 -0300

    No anda el ruteo

--- Front-End/TpfinalFrontEntorno/src/app/components/shared/footer/footer.component.ts
+++ Front-End/TpfinalFrontEntorno/src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { TaskService } from 'src/app/services/auth/task.service';
+import { MenuadminComponent } from '../../menu/menuadmin/menuadmin.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  visible: boolean = true;
+  @Output() menu: EventEmitter<any> = new EventEmitter();
+  @Output() Logout: EventEmitter<any> = new EventEmitter();
+  @Input() menunav:any;
+  @Input() linkprincipal:any;
+  @Input() linkperfil:any;
+  
+  constructor(private task: TaskService, private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+  LogoutEmitter(){
+    this.Logout.emit(true);
   }
 
 }

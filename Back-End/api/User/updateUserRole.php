50ce32b9d6d4220c1e0df497687ffb89f2c9cc53
Pablo <ciberpablo12@hotmail.com>
2021-11-08 20:13:34 -0300

    algunos archivos que me faltaron

---
+++ Back-End/api/User/updateUserRole.php
@@ -0,0 +1,77 @@
+<?php
+include_once("../../Config/db.php");
+include_once('../../middleware/genericMethod.php');
+include_once('../../Config/constant.php');
+include_once('../../Helpers/Security/Securitypass.php');
+include_once('../../middleware/mail.php');
+
+$_PUT = json_decode(file_get_contents('php://input'), true);
+
+$user = new UpdateUserRole($_PUT["id"],$_PUT['idRole'],$_PUT["firstName"],$_PUT["lastName"],$_PUT["mail"],$_PUT["address"],$_PUT["phone"],"","",$_PUT["idDocumentType"],$_PUT["docNumber"],"","",0);
+
+$user->UpdateUserRole($_GET['id'],$_PUT['idRole']);
+
+class UserResult {
+    public $result;
+    public $message;
+    public $status;
+}
+
+class UpdateUserRole extends genericMethod
+{
+    private $id;
+    private $idRole;
+    private $idDocumentType;
+    private $firstName;
+    private $lastName;
+    private $mail;
+    private $docNumber;
+    private $address;
+    private $phone;
+    private $userName;
+    private $userPass;
+    private $creationDate;
+    private $finalDate;
+    private $state;
+
+    public function __construct($id, $idRole, $firstName, $lastName, $mail, $address, $phone, $userName, $userPass, $idDocumentType, $docNumber, $creationDate, $finalDate, $state)
+    {
+        #region initial
+        $this->id = $id;
+        $this->idRole = $idRole;
+        $this->idDocumentType = $idDocumentType;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->mail = $mail;
+        $this->docNumber = $docNumber;
+        $this->address = $address;
+        $this->phone = $phone;
+        $this->userName = $userName;
+        $this->userPass = $userPass;
+        $this->creationDate = $creationDate;
+        $this->finalDate = $finalDate;
+        $this->state = $state;
+        #endregion
+    }
+
+    public static function UpdateUserRole($id, $idRole)
+    {
+        $response = new UserResult();
+        try {
+            $cnn = Connection();
+            $result = mysqli_query($cnn, "update users set idRole =" . $idRole . " where id =" . $id);
+            if ($result) {
+                $response->status = SUCCESS_RESPONSE;
+                $response->message = "El rol del usuario fue cambiado con exito.";
+            } else {
+                $response->status = SUCCESS_RESPONSE;
+                $response->message = "El  rol del usuario no fue cambiado con exito.";
+            }
+            echo json_encode($response);
+        } catch (\Exception $e) {
+            $response->status = SUCCESS_RESPONSE;
+            $response->message = $e->getMessage();
+            echo json_encode($response);
+        }
+    }
+}
---
+++ Back-End/api/role/delete.php
@@ -0,0 +1,36 @@
+<?php
+include_once("../../Config/db.php");
+include_once('../../middleware/genericMethod.php');
+include_once('../../Config/constant.php');
+include_once('../../Helpers/Security/Securitypass.php');
+
+class ResultRole {
+    public $result;
+    public $message;
+}
+
+$cnn = Connection();
+
+$response = new ResultRole();
+
+$id = json_decode(file_get_contents('php://input'), true);
+
+$id = ((int)$id);
+
+if(!$id)
+{
+    return http_response_code(400);
+}
+
+$result = mysqli_query($cnn,"update Roles set state = 2 where id =".$id);
+
+if($result){
+    $response->result = 'Ok';
+    $response->message="El permiso fue eliminado con exito";
+}
+else{
+    $response->result = 'Error';
+    $response->message="El permiso no fue eliminado con exito";
+}
+
+echo json_encode($response);
